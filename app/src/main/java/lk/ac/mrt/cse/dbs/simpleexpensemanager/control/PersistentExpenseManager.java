f13b014b5192f4bebe58ad5b9b8cf6352ad00fb8
Basgaran <maathangi2017@gmail.com>
2021-02-03 18:12:54 +0530

    Updated Version

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -0,0 +1,56 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.InDbAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.InDbTransactionDAO;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class PersistentExpenseManager extends ExpenseManager{
+
+
+    Context context;
+    public PersistentExpenseManager(Context ctx)  {
+        context=ctx;
+        try {
+
+            setup();
+        } catch (ExpenseManagerException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+
+    public void setup() throws ExpenseManagerException {
+
+        SQLiteDatabase db = context.openOrCreateDatabase("180071P", context.MODE_PRIVATE, null);
+
+        // create the databases.
+        db.execSQL("CREATE TABLE IF NOT EXISTS Account(" +
+                "accountNo VARCHAR PRIMARY KEY," +
+                "bankName VARCHAR," +
+                "accountHolderName VARCHAR," +
+                "balance REAL" +
+                " );");
+
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS Account_Transaction(" +
+                "Transaction_id INTEGER PRIMARY KEY," +
+                "accountNo VARCHAR," +
+                "expenseType INT," +
+                "amount REAL," +
+                "date DATE," +
+                "FOREIGN KEY (accountNo) REFERENCES Account(accountNo)" +
+                ");");
+
+
+
+
+        InDbAccountDAO accountDAO = new InDbAccountDAO(db);
+
+        setAccountsDAO(accountDAO);
+
+        setTransactionsDAO(new InDbTransactionDAO(db));
+
+        /*** End ***/
+    }
+}
